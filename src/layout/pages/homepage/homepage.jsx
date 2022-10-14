dd488bf95aa028d7a6527d5ee671687726b4b519
Anas Alam <anasalam027@gmail.com>
2022-10-15 00:23:22 +0530

    hover effect added

--- src/layout/pages/homepage/homepage.jsx
+++ src/layout/pages/homepage/homepage.jsx
@@ -4,16 +4,20 @@ import "./homepage.css";
 import TopSection from "./topSection/topSection";
 import DoubleImageText from "../homepage/doubleImageText/doubleImageText";
 import Slider from "../homepage/slider/slider";
+import Footer from "../../components/footer/footer";
+import CategorySec from "./category-sec/category-sec";
+import FabricSec from "./Fabric-sec/fabric-sec";
 function Homepage() {
   return (
     <div className="Homepage">
       <TopSection />
       <Slider heading="NEW ARRIVALS" />
+      <CategorySec />
       <Slider heading="Top wear" />
       <Slider heading="best sellers" />
+      <FabricSec />
       <Slider heading="bottoms" />
       <Slider heading="trending" />
-
       <DoubleImageText />
     </div>
   );
