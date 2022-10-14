d314b26454ba7dbd2318ce840b885aa4b1429938
Anas Alam <anasalam027@gmail.com>
2022-10-14 16:48:49 +0530

    card

--- src/layout/pages/homepage/homepage.jsx
+++ src/layout/pages/homepage/homepage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ProductCard from "../../components/productCard/productCard";
 import "./homepage.css";
 function Homepage() {
-  return <div className="Homepage"></div>;
+  return (
+    <div className="Homepage">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
 }
 
 export default Homepage;
